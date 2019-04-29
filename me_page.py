ec6409dd11ad422703eb56527b4ad28854f26807
Andreas Arnesson <andreas_osby@hotmail.com>
2019-04-29 11:28:01 +0200

    missed a validateion error

--- me_page.py
+++ me_page.py
@@ -1,7 +1,8 @@
 from app import create_app, db
 from app.models import User, Post
+from app.config import DevConfig
 
-app = create_app()
+app = create_app(DevConfig)
 
 @app.shell_context_processor
 def make_shell_context():
