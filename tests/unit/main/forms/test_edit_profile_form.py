b491c6ae31814bc9759d061188d3d836ba63e75c
Andreas Arnesson <andreas_osby@hotmail.com>
2019-04-29 19:32:53 +0200

    unit test for main/forms

---
+++ tests/unit/main/forms/test_edit_profile_form.py
@@ -0,0 +1,31 @@
+"""
+Contains tests for app.main.forms.EditProfileForm class
+"""
+import pytest
+from unittest import mock
+from wtforms.validators import ValidationError
+from app.main.forms import EditProfileForm
+
+
+
+@mock.patch("app.main.forms.User")
+def test_validate_username(mock_user, app):
+    first = mock.Mock()
+    mock_user.query.filter_by.return_value = first
+    first.first.return_value = None
+    username = mock.Mock()
+    username.data = "john"
+    assert EditProfileForm(username.data).validate_username(username) == None
+    assert EditProfileForm("susan").validate_username(username) == None
+
+
+
+@mock.patch("app.main.forms.User")
+def test_validate_username_raise_exception(mock_user, app):
+    first = mock.Mock()
+    mock_user.query.filter_by.return_value = first
+    first.first.return_value = "john"
+    username = mock.Mock()
+    username.data = "john"
+    with pytest.raises(ValidationError):
+        EditProfileForm("susan").validate_username(username)
