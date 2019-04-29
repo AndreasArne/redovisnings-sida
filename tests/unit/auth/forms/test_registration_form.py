80086930fd8d6004db0214a8223c7fc17f77c4fa
Andreas Arnesson <andreas_osby@hotmail.com>
2019-04-29 19:14:30 +0200

    unit test for auth/forms

---
+++ tests/unit/auth/forms/test_registration_form.py
@@ -0,0 +1,54 @@
+"""
+Contains tests for app.auth.forms.RegistrationForm class
+"""
+import pytest
+from unittest import mock
+from wtforms.validators import ValidationError
+from app.auth.forms import RegistrationForm
+
+
+
+@mock.patch("app.auth.forms.User")
+def test_validate_username(mock_user, app):
+    first = mock.Mock()
+    mock_user.query.filter_by.return_value = first
+    first.first.return_value = None
+    username = mock.Mock()
+    username.data = "john"
+    assert RegistrationForm().validate_username(username) == None
+
+
+
+@mock.patch("app.auth.forms.User")
+def test_validate_username_raise_exception(mock_user, app):
+    first = mock.Mock()
+    mock_user.query.filter_by.return_value = first
+    first.first.return_value = "john"
+    username = mock.Mock()
+    username.data = "john"
+    with pytest.raises(ValidationError):
+        RegistrationForm().validate_username(username)
+
+
+
+@mock.patch("app.auth.forms.User")
+def test_validate_email(mock_user, app):
+    first = mock.Mock()
+    mock_user.query.filter_by.return_value = first
+    first.first.return_value = None
+    email = mock.Mock()
+    email.data = "john@gmail.com"
+    assert RegistrationForm().validate_email(email) == None
+
+
+
+@mock.patch("app.auth.forms.User")
+def test_validate_email_raise_exception(mock_user, app):
+    first = mock.Mock()
+    mock_user.query.filter_by.return_value = first
+    first.first.return_value = "john@gmail.com"
+    email = mock.Mock()
+    email.data = "john@gmail.com"
+    with pytest.raises(ValidationError):
+        RegistrationForm().validate_email(email)
+
