63eb3b845fcebc1bae5d24ea1a3790353ecdba17
Andreas Arnesson <andreas_osby@hotmail.com>
2019-05-02 15:32:24 +0200

    integration test main routes

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,7 +1,7 @@
 """
 Fixtures for integration tests
 """
-# pylint: disable=redefined-outer-name
+# pylint: disable=redefined-outer-name,unused-argument
 import pytest
 
 @pytest.fixture
@@ -18,6 +18,19 @@ def user_dict():
     return data
 
 
+
+@pytest.fixture
+def post_dict():
+    """
+    Dictionary with post data
+    """
+    data = {
+        "post": "This is my first post",
+    }
+    return data
+
+
+
 @pytest.fixture
 def register_user_response(client, user_dict):
     """
@@ -26,6 +39,34 @@ def register_user_response(client, user_dict):
     response = client.post(
         '/register',
         data=user_dict,
+        follow_redirects=True,
+    )
+    return response
+
+
+
+@pytest.fixture
+def login_user_response(client, register_user_response, user_dict):
+    """
+    Register user from user_dict
+    """
+    response = client.post(
+        '/login',
+        data=user_dict,
+        follow_redirects=True,
+    )
+    return response
+
+
+
+@pytest.fixture
+def user_post_response(client, login_user_response, user_dict, post_dict):
+    """
+    Register user from user_dict
+    """
+    response = client.post(
+        "/",
+        data=post_dict,
         follow_redirects=True,
     )
     return response
